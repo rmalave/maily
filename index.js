6034c10d9bf1917e3d0ab3ece2180bcb0ef73b69
Rafael Malave <rafomalave@gmail.com>
2018-03-25 12:38:05 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send({ hi: 'there'});
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log('Tuning in to port 3000');
+});
